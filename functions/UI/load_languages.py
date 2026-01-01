c68c3a6b7c9af95384c7bbf3b926825acd56c0eb
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-01-01 19:40:45 +0330

    selection Use languages

--- functions/UI/load_languages.py
+++ functions/UI/load_languages.py
@@ -1,25 +1,10 @@
 def load_languages(list_of_langueges = list):
-    '''This function, get a list and return
-        all condition that you can make with that.
-    Args:
-        lsit_of_langueges (list)
-    Return:
-        list: a list from Args * 2 
-            from list with 2 objects
-    Example:
-        >>>load_languages([farsi, english])
-        [[farsi, english], [english, farsi]]
-    '''
-    list_all_conditions = list()
+    for i in range(2):
+        if i == 0:
+            print("Please Enter the origin langueage")
+        else:
+            print("Please Enter the destination langueage")
+        for index,origin in enumerate(list_of_langueges):
+            print(f"{index}.{origin}")
+        
 
-    for index, object in  enumerate(list_of_langueges):
-        my_pop = list_of_langueges.pop(index)
- 
-        for other in list_of_langueges:
-            list_all_conditions.append([object, other])
-
-
-
-        list_of_langueges.insert(index, my_pop)
-
-    return list_all_conditions
