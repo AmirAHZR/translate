58cdf92e011568c50cbacfab361d99653a6fb393
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-01-01 20:02:47 +0330

    selection Use languages finally

--- functions/UI/load_languages.py
+++ functions/UI/load_languages.py
@@ -1,10 +1,14 @@
 def load_languages(list_of_langueges = list):
-    for i in range(2):
-        if i == 0:
-            print("Please Enter the origin langueage")
-        else:
-            print("Please Enter the destination langueage")
-        for index,origin in enumerate(list_of_langueges):
-            print(f"{index}.{origin}")
+    for ind, obj in enumerate(list_of_langueges):
+        print(f"{ind +1}.{obj}")
+    origin_num = int(input())
+    origin = list_of_langueges.pop(origin_num-1)
+    for ind, obj in enumerate(list_of_langueges):
+        print(f"{ind +1}.{obj}")
+    destination = int(input())
+    list_of_langueges.insert(origin_num-1,origin)
+    return origin, destination
+
         
+
 
