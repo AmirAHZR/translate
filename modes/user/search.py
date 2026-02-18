a6af3b972388e7dd47ce35270c4ed599c4346c0a
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-02-18 21:35:50 +0330

    debug index Erorr

--- modes/user/search.py
+++ modes/user/search.py
@@ -3,9 +3,10 @@ from document.languages_words import langs, list_lang
 from functions.filtering.word_filtering import filtering
 from functions.translate.translate import *
 def user_mode():
-    origin, des = functions.UI.load_languages.load_languages(list(list_lang))
-    
-    word = input()
+    origin, destination = functions.UI.load_languages.load_languages(list_lang)
 
-    result = translate(filtering(word), langs[list_lang[origin]], langs[list_lang[des]])
-    return result, origin, word
+
+    word = input()
+    
+    result = translate(filtering(word), langs[origin], langs[destination])
+    return result, origin, word, langs
